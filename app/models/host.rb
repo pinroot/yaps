a1d189f584fa4cd39700b1d4b3a86e9971ae693b
Roman Kolesnikov <krn@pinroot.org>
2021-12-25 04:27:09 +0700

    Made the test pass again!

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,3 +1,9 @@
 class Host < ApplicationRecord
   validates_presence_of :name, :fqdn
+
+  validates :fqdn, format: {
+    with:    %r{[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$}i, multiline: true,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+
 end
