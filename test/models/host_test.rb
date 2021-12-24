f134af25c7137323a5154528f67dd4ad0d85f5db
Roman Kolesnikov <krn@pinroot.org>
2021-12-25 03:38:07 +0700

    Add first test

--- test/models/host_test.rb
+++ test/models/host_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class HostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Host attributes must not be empty" do
+    host = Host.new
+    assert host.invalid?
+    assert host.errors[:name].any?
+    assert product.errors[:fqdn].any?
+  end
+
 end
