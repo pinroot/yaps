647128fa8927484589087566422f930c03bfb04b
Roman Kolesnikov <krn@pinroot.org>
2021-12-25 04:29:17 +0700

    Update the message

--- app/models/host.rb
+++ app/models/host.rb
@@ -3,7 +3,7 @@ class Host < ApplicationRecord
 
   validates :fqdn, format: {
     with:    %r{[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$}i, multiline: true,
-    message: 'must be a URL for GIF, JPG or PNG image.'
+    message: 'must be a FQDN'
   }
 
 end
