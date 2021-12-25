442713fbd5db47b63ba88e2c33cc7f4a888f52da
Czepesch <czepesch@gmail.com>
2021-12-25 14:18:05 +0100

    fix association in host model

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,6 +1,6 @@
 class Host < ApplicationRecord
   validates_presence_of :name, :fqdn
-  has_many :cheks
+  has_one :cheks
 
   validates :fqdn, format: {
     with:    %r{[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$}i, multiline: true,
