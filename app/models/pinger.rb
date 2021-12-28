dccbe91f715a5894183545bf138af0857985f452
Roman Kolesnikov <krn@pinroot.org>
2021-12-28 10:49:50 +0700

    delete regex examples

--- app/models/pinger.rb
+++ app/models/pinger.rb
@@ -3,9 +3,6 @@ class Pinger < ApplicationRecord
   # 1) MINIMUM\MAXIMUM INTERVAL MUST STORED IN APP SETTINGS
   # 2) STORAGE DEPTH FOR EVENTS (IN DAYS) MUST STORED IN APP SETTINGS
 
-  valid_ip_address_regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
-  valid_hostname_regex = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/
-
   after_create :create_pinger_scheduler
   after_update :update_pinger_scheduler
   after_destroy :destroy_pinger_scheduler
