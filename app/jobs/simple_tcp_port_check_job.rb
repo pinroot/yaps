18e1210863de06bfe5a4b9a4f45e4a2e216ad332
Roman Kolesnikov <krn@pinroot.org>
2022-01-21 21:44:51 +0700

    Implement Pinger Jobs

---
+++ app/jobs/simple_tcp_port_check_job.rb
@@ -0,0 +1,27 @@
+class SimpleTcpPortCheckJob
+  include SuckerPunch::Job
+  def perform(pinger_id)
+    @pinger = Pinger.find(pinger_id)
+
+    @last_event = @pinger.events.last
+    
+    @checker = HostPinger::TCPChecker.new(@pinger.address, @pinger.port, @pinger.timeout)
+
+    def set_reason
+      if @checker.up?
+        return "Connection allowed"
+      elsif @checker.down?
+        return @checker.exception
+      else
+        return "Unknown"
+      end
+    end
+
+    if @checker.status != @last_event.status
+      @pinger.events.build(status: @checker.status, reason: set_reason, response_time: @checker.response_time).save
+    end
+
+    @pinger.update_columns(pinged_at: Time.now)
+
+  end
+end
